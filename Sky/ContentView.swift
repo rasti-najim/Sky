89bda9ba1ff51d0fdc5b5eeda36695f41c255733
rasti_najim <rastirasheed@gmail.com>
2022-03-14 21:37:11 -0400

    Initial Commit

---
+++ Sky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sky
+//
+//  Created by rasti najim on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sky/SkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyApp.swift
+//  Sky
+//
+//  Created by rasti najim on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
